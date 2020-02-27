652490fce3d1c191d2357f53c033e60f3873ce99
Mark Heckler <mark.heckler@gmail.com>
2020-02-26 23:03:22 -0500

    Initial commit

---
+++ src/main/java/com/thehecklers/ssecressvr/SsecResSvrApplication.java
@@ -0,0 +1,56 @@
+package com.thehecklers.ssecressvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class SsecResSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsecResSvrApplication.class, args);
+	}
+
+}
+
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests().mvcMatchers("/claims").hasAuthority("SCOPE_openid")
+				.and()
+				.authorizeRequests().mvcMatchers("/email").hasAuthority("SCOPE_email")
+				.and()
+				.oauth2ResourceServer().jwt();
+	}
+}
+
+
+@RestController
+@RequestMapping("/resources")
+class ResourceController {
+	@GetMapping("/something")
+	String getSomething() {
+		return "This is really something!";
+	}
+
+	@GetMapping("/claims")
+	Map<String, Object> getClaims(@AuthenticationPrincipal Jwt jwt) {
+		return jwt.getClaims();
+	}
+
+	@GetMapping("/email")
+	String getSubject(@AuthenticationPrincipal Jwt jwt) {
+		return jwt.getSubject();
+	}
+}
---
+++ src/test/java/com/thehecklers/ssecressvr/SsecResSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.ssecressvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsecResSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
